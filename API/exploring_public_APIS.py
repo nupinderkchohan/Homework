b55c3a92c463715d5de7492927fe554486c011ca
nupinderkchohan <nupinderkchohan@gmail.com>
2026-08-13 15:58:36 -0700

    API

---
+++ API/exploring_public_APIS.py
@@ -0,0 +1,27 @@
+import requests
+
+
+
+# Example of fetching a random joke
+
+url = "https://official-joke-api.appspot.com/random_joke"
+
+
+
+# Send GET request to fetch a joke
+
+response = requests.get(url)
+
+
+
+# Check if the request was successful
+
+if response.status_code == 200:
+
+    joke_data = response.json()
+
+    print(f"Joke: {joke_data['setup']} - {joke_data['punchline']}")
+
+else:
+
+    print(f"Failed to retrieve joke. Status code: {response.status_code}")
